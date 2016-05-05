faf02c071ebc3fca309528e2e508b06ca090919a
shadow walker <matijakovacek8@gmail.com>
2016-05-05 18:13:27 +0100

    Debuging layers need soring for inputs

--- Chas Project/Assets/Matthews Team/Scripts/dream_scripts/DreamUtil.cs
+++ Chas Project/Assets/Matthews Team/Scripts/dream_scripts/DreamUtil.cs
@@ -4,20 +4,29 @@ using System.Collections;
 public class DreamUtil : BaseDreamClass
 {
     [HideInInspector]
-    GameObject game_utility_; 
-	// Use this for initialization
-	void Start ()
+    GameObject game_utility_;
+
+    [HideInInspector]
+    public bool is_destroyable_;
+    // Use this for initialization
+    void Start ()
     {
         game_utility_ = GameObject.FindGameObjectWithTag("GameUtility");
+
         this.GetComponent<Rigidbody2D>().velocity = (this.transform.up * this.Speed);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch(0).position)), transform.forward, Mathf.Infinity);
+            if (hit)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
             if (hit.collider == this.gameObject.GetComponent<Collider2D>())
             {
                 game_utility_.GetComponent<Points>().Score = (game_utility_.GetComponent<Points>().Score + this.Points);   
@@ -34,4 +43,27 @@ public class DreamUtil : BaseDreamClass
             Debug.Log("end game");
         }
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Target")
+        {
+            is_destroyable_ = true;
+        }      
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Target")
+        {
+            is_destroyable_ = false;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Target")
+        {
+            is_destroyable_ = true;
+        }
+    }
+
 }
+
