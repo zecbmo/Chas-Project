a65aa95048133167d7ce3f2e8b329e212aeff9a8
StewMcc <stewmcc@gmail.com>
2016-05-17 20:55:12 +0100

    Basic Scrolling background and start of character controls

---
+++ Chas Project/Assets/Stews/Scripts/MoveItCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MovementType {    
+    Land,
+    Sea,
+    Air
+}
+
+public class MoveItCharacter : MonoBehaviour {
+
+    public MovementType movementType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Chas Project/Assets/Stews/Scripts/MoveItCharacterController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveItCharacterController : MonoBehaviour {
+
+    public MoveItCharacter character;
+    private Vector2 forceDirection;
+	// Use this for initialization
+	void Start () {
+        forceDirection = GetForceDirection();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i =0; i <Input.touchCount; i++) {
+            if(Input.GetTouch(i).phase == TouchPhase.Began) {
+                Debug.Log("tapped");
+                character.GetComponent<Rigidbody2D>().AddForce(forceDirection);
+            }
+        }
+	}
+
+    Vector2 GetForceDirection() {
+        Vector2 force_direction;
+        switch (character.movementType) {
+            case MovementType.Air: {
+                    force_direction = new Vector2(0, 1);
+                    break;
+                }
+            case MovementType.Land: {
+                    force_direction = new Vector2(0, 1);
+                    break;
+                }
+            case MovementType.Sea: {
+                    force_direction = new Vector2(0, -1);
+                    break;
+                }
+            default:
+                break;
+        }
+
+        return Vector2.zero;
+    }
+}
