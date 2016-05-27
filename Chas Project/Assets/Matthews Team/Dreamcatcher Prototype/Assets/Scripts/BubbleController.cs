9933f80f1ffd2cc655682863fee1fa94e5f9441e
Chris McEvoy <cmcevoy623@outlook.com>
2016-05-27 11:21:15 +0100

    This is my early dreamcatcher prototype. There's still a lot to do with it but I'm including it so that code chunks may be copied from it if needed. I've put this in the Matthew's team folder branch but it would probably need to be copied to its own separate folder outside the project folder in order to work.

---
+++ Chas Project/Assets/Matthews Team/Dreamcatcher Prototype/Assets/Scripts/BubbleController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class BubbleController : MonoBehaviour {
+
+    public static float increaseRate;
+
+    private float centreX = 0f; //used as a centre point, can test for trajectory paths (which direction to move in)
+
+    private float minScale = 0.1f; //minimum and maximum relative sizes of the dreams
+    private float maxScale = 2f; //max scale before popping
+    private float scale; //the size of the bubble
+
+    private float minY = -2.75f; //coordinate ranges for where the bubble can be within
+    private float maxY = 3.25f;
+    private float minX = -2.75f;
+    private float maxX = 2.75f;
+
+    private float movementSpeed; 
+    private float rightScale; //movement scales
+    private float upScale;
+
+    private Rigidbody2D rb2D;
+    private float spawnPositionX;
+
+    int direction = 0; //right or left
+
+    // Use this for initialization
+    void Awake()
+    {
+        //initialisation
+        spawnPositionX = this.transform.position.x;
+        increaseRate = 0f;
+        rb2D = GetComponent<Rigidbody2D>();
+        scale = minScale;
+        movementSpeed = 1f * (GameManagerController.growthRate); //movement speed relates to growth rate
+        SetDirection(); //set horizontal direction
+        UpdateScale();
+	}
+	
+    void UpdateScale()
+    {
+        scale = scale + (Time.deltaTime * GameManagerController.growthRate); //update scale over time
+        transform.localScale = new Vector3(scale, scale); //increase size
+        if (scale >= maxScale)
+        {
+            BubblePop(); //pop if too big
+        }
+    }
+
+    void BubblePop()
+    {
+        GameManagerController.bubbleNo--; //decrease number of bubbles
+
+        Destroy(this.gameObject); //destroy bubble
+    }
+
+    void MoveBubble()
+    {
+        rb2D.AddForce(transform.up * upScale, ForceMode2D.Force); //upwards force
+        rb2D.AddForce(transform.right * direction * rightScale, ForceMode2D.Force); //right or left force
+    }
+
+    void SetDirection() //determines whether the bubble will move right or left
+    {
+        float chanceLeft = Random.Range(0, 100);
+        if (chanceLeft < 50)
+        {
+            direction = -1; //left
+        }
+        else
+        {
+            direction = 1; //right
+        }
+        upScale = Random.Range((movementSpeed * 0.8f), movementSpeed); //for variety, slightly random upwards speed
+        rightScale = Random.Range(0, movementSpeed); //random horizontal speed
+    }
+
+    void CalculateHorizontalVelocity()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        BubblePop(); //if clicked, pop bubble
+    }
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        UpdateScale(); //scale updated every frame
+        MoveBubble(); //bubble position updated every frame
+	}
+}
---
+++ Chas Project/Assets/Matthews Team/Dreamcatcher Prototype/Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerController : MonoBehaviour {
+
+    public static int bubbleNo; //number of bubbles on screen
+    public static float growthRate;
+
+    public GameObject goodBubble; 
+    public GameObject badBubble; 
+
+    private GameObject bubble; //bubble becomes either good or bad
+
+    private int chanceGood; //percentage chance the next bubble will be good (may use this to change chance later on?)
+    private bool isMaxSpawn; //is there the max number of bubbles on screen?
+    //private int maxSpawnInt; //max number of bubbles
+
+    private float minY = -2.75f; //coordinate ranges for where the bubble can be within
+    private float minX = -2.75f; //spawn new bubbles at minY and between min and max Xs
+    private float maxX = 2.75f;
+
+    private float time; //used to calculate random spawn time between min and max times
+    private float spawnTime;
+    private float minTime;
+    private float maxTime;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //initialisation
+        isMaxSpawn = false; //spawn stuff
+        //maxSpawnInt = 4;
+        chanceGood = 50;
+
+        bubbleNo = 0;
+
+        minTime = 2f; //time stuff
+        maxTime = 4f;
+
+        time = minTime;
+        SetRandomTime(); //set random spawn time for first bubble
+
+        ChooseNextBubble(); //choose first bubble
+
+        growthRate = 0.30f;
+        SpawnBubble();
+        //InvokeRepeating("IncreaseDifficulty", 2f, 2f); //increase growth rate and reduce spawn time every 2secs // could be made public var
+        //InvokeRepeating("IncreaseMaxSpawn", 15f, 15f); //increase max number of bubbles every 15 seconds //could be made public var
+        //InvokeRepeating("IncreaseGrowthRate", 1 / 60, 1 / 60);
+        InvokeRepeating("SpawnBubble", 2f, 2f);
+    }
+
+    void IncreaseGrowthRate()
+    {
+        growthRate = growthRate + Time.deltaTime * 4;
+    }
+
+    void ChooseNextBubble()
+    {
+        int choice = Random.Range(0, 100); //random int between 0 and 100 (less than 100)
+
+        if (choice < chanceGood) //if 0-49 (half the possible numbers and therefore 50% chance)
+        { bubble = goodBubble; } //bubble either becomes good or bad
+        else
+        { bubble = badBubble; }
+
+        /*if (bubbleNo < maxSpawnInt) //determines whether we are at the spawn limit or not
+        {
+            isMaxSpawn = false;
+        }
+        else
+        {
+            isMaxSpawn = true;
+        }*/
+    }
+
+    Vector2 SetSpawnPoint()
+    {
+        float spawnX = Random.Range(minX, maxX); //random position between min and max x
+        float yOffset = Random.Range(-0.25f, 0.25f); //to slightly vary the y position
+        Vector2 spawnPoint = new Vector2(spawnX, minY + yOffset); //spawn at random point
+
+        return spawnPoint;
+    }
+
+    void SpawnBubble()
+    {
+        //time = 0; //reset time
+
+        ChooseNextBubble(); //next bubble chosen
+
+        if (isMaxSpawn == false) //if spawning is possible
+        {
+            Instantiate(bubble, SetSpawnPoint(), Quaternion.identity); //spawn bubble at point
+
+            bubbleNo++; //increase numbers
+        }
+    }
+
+    void SetRandomTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime); //chooses random time
+    }
+
+    void IncreaseDifficulty() //placeholder
+    {
+        /*float reduxVal = 0.1f;
+        growthRate = growthRate + 0.025f; //placeholder code
+        if (minTime < reduxVal) //placeholder code
+        {
+            minTime = reduxVal; //reduxVal becomes the minimum time possible, no more subtraction
+        }
+        else
+        {
+            minTime = minTime - reduxVal;
+        }
+        if (maxTime < reduxVal * 2) //placeholder code
+        {
+            maxTime = reduxVal * 2;
+        }
+        else
+        {
+            maxTime = maxTime - reduxVal;
+        }*/
+    }
+
+    /*void IncreaseMaxSpawn()
+    {
+        maxSpawnInt = maxSpawnInt + 1; //increase max spawn number
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        /*
+        //Counts up
+        time += Time.deltaTime; //this is necessary, trust me
+
+        //Check if its the right time to spawn the object
+        if (time >= spawnTime) //if spawning is possible
+        {
+            SpawnBubble(); 
+            SetRandomTime(); //set new random time
+        }*/
+        Time.timeScale += 0.0005f;
+        // 317 frames before first pop
+    }
+
+}
