21d925858f91a0ebcee6d5fde902471a9fbc9ae1
StewMcc <stewmcc@gmail.com>
2016-06-15 02:07:31 +0100

    New Obstacle Spawner

---
+++ Chas Project/Assets/Stews/Scripts/MoveItObstacle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveItObstacle : MonoBehaviour {
+    [Tooltip("Distance from centre to edge")]
+    public float SeperationHorizontal = 0.0f;
+    [Tooltip("Maximum possible variation in height (Must be higher than min)")]
+    public float MaxHeightVariation = 0.0f;
+    [Tooltip("Minimum possible variation in height")]
+    public float MinHeightVariation = 0.0f;    
+    [Tooltip("Chance of obstacle turning up")]
+    [Range(0, 100)]
+    public float Weight = 100.0f;
+
+    public void SetSpeed(float obstacleSpeed) {
+        speed = obstacleSpeed;
+        speedUpdated = true;
+    }
+
+    private float speed = 1;
+    private Vector2 forceDirection = new Vector2(-1,0);
+    private Rigidbody2D characterRigidBody;
+
+    bool speedUpdated = false;
+        
+    void Start() {        
+        characterRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        characterRigidBody.velocity = (forceDirection * speed);
+    }
+        
+    void Update () {
+        if (speedUpdated) {
+            characterRigidBody.velocity = (forceDirection * speed);
+        }
+    }
+
+    void OnValidate() {
+        // Check if max is higher than min
+        if (MaxHeightVariation < MinHeightVariation) {
+            MaxHeightVariation = MinHeightVariation;
+        }
+    }
+}
---
+++ Chas Project/Assets/Stews/Scripts/MoveItObstacleSpawner.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoveItObstacleSpawner : MonoBehaviour {
+
+    [Tooltip("Speed obstacles move at.")]
+    public float ObstacleSpeed = 20.0f;
+
+    public List<MoveItObstacle> spawnableObstacles = new List<MoveItObstacle>();
+
+    private MoveItObstacle currentObstacle = null;
+    private MoveItObstacle nextObstacle = null;
+    private float totalWeight = 0;
+    // Use this for initialization
+    void Start() {
+        totalWeight = GetListTotalWeight();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentObstacle == null) {
+            SpawnObstacle(GetObstacle());
+            nextObstacle = GetObstacle();
+        } else {
+            float distance = Vector3.Distance(currentObstacle.transform.position, transform.position);
+            float requiredDistance = currentObstacle.GetComponent<MoveItObstacle>().SeperationHorizontal + nextObstacle.GetComponent<MoveItObstacle>().SeperationHorizontal;
+            if (distance > requiredDistance) {
+                SpawnObstacle(nextObstacle);
+                nextObstacle = GetObstacle();
+            }
+        }
+    }
+
+    MoveItObstacle GetObstacle() {
+        MoveItObstacle newObstacle = null;
+        float randomNumber = Random.Range(0.0f, totalWeight);
+
+        foreach (MoveItObstacle obstacle in spawnableObstacles) {
+            // find the 
+            if (randomNumber < obstacle.Weight) {
+                newObstacle = obstacle;
+                break;
+            }
+            randomNumber -= obstacle.Weight;
+
+        }
+        return newObstacle;
+    }
+
+    float GetListTotalWeight() {
+        float total = 0.0f;
+        foreach (MoveItObstacle obstacle in spawnableObstacles) {
+            total += obstacle.Weight;
+        }
+        return total;
+    }
+
+    void SpawnObstacle(MoveItObstacle newObstacle) {        
+        currentObstacle = Instantiate(newObstacle, transform.position, Quaternion.identity) as MoveItObstacle;        
+    }
+}
