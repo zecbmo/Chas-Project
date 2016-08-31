3ef8fd6c45a5ebf8d2e8999a0178446184760957
Stewart L. McCready <stewmcc@gmail.com>
2016-08-31 17:59:15 +0100

    Half way trhough Bouyancy, testing. IGDA Save.

---
+++ Chas Project/Assets/Stews/Scripts/WaterInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class WaterInfo : MonoBehaviour {
+    [SerializeField]
+    private Transform waterLevel;
+    [SerializeField]
+    private Transform waterBed;
+    [SerializeField]
+    private float waterHeight;
+
+    public void Awake() {
+        waterHeight = (waterBed.transform.position - waterLevel.transform.position).magnitude;
+    }
+#if UNITY_EDITOR
+    public void Update() {
+        waterHeight = (waterBed.transform.position - waterLevel.transform.position).magnitude;
+
+    }
+#endif
+}
